76e1d6fee5c09f0c873f42d3e3048bd3045569a2
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-19 19:38:40 +0300

    Group organisation

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl">Home</h1>
+      <h1 className="text-2xl underline mb-3">Home</h1>
       <Button>Click me</Button>
     </div>
   )
