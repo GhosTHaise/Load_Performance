6f9773d5dae3b37d88e5fc362faf92742112d354
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-22 17:29:01 +0300

    show banner

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,11 +2,23 @@ import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="min-h-screen">
-      {/** 
-      <h1 className="text-2xl underline mb-3">Home</h1>
-      <Button>Click me</Button>
-      */}
-    </div>
+    <main
+      className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col"
+    >
+      <section
+        className="nav-padding w-full"
+      >
+        <div
+          className="flex-center relative min-h-[274px] w-full flex-col rounded-xl
+           bg-banner bg-cover bg-center  text-center"
+        >
+          <h1
+            className="sm:heading1 heading2 mb-6  text-center text-white"
+          >
+             JavaScript Mastery Resources
+          </h1>
+        </div>
+      </section>
+    </main>
   )
 }
