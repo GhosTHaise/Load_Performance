9c94fbe28278170c143f23085a032be129fe651d
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-25 17:27:08 +0300

    select field with groq

--- sanity/action.ts
+++ sanity/action.ts
@@ -1,5 +1,6 @@
 import { groq } from "next-sanity";
 import { readclient } from "./lib/client";
+import { buildQuery } from "./utils";
 
 interface GetResourcesParams {
     query : string;
@@ -12,9 +13,24 @@ export const getResources = async (params : GetResourcesParams) => {
 
     try {
         const resources = await readclient.fetch(
-            groq``
-        )
+            groq`${buildQuery({
+                type:"ressource",
+                query,
+                category,
+                page : parseInt(page)
+            })}{
+                title,
+                _id,
+                downloadLink,
+                "image": poster.asset->url,
+                views,
+                slug,
+                category
+            }`
+        );
+
+        return resources;
     } catch (error) {
-        
+        console.log(error);
     }
 }
