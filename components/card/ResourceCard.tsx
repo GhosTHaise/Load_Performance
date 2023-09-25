ee5ab70137a2de941bee3844a814bcad77ecc0a1
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-25 19:20:45 +0300

    render a card with image

--- components/card/ResourceCard.tsx
+++ components/card/ResourceCard.tsx
@@ -6,6 +6,8 @@ import {
     CardHeader,
     CardTitle,
 } from "@/components/ui/card"
+import Image from "next/image";
+import Link from "next/link";
   
 interface Props {
     title : string;
@@ -22,7 +24,34 @@ const ResourceCard = ({
     slug
 } : Props) => {
   return (
-    <div>ResourceCard</div>
+    <Card
+        className="w-full max-w-fit border-0 !bg-transparent sm:max-w-[356px]"
+    >
+        <Link
+            href={`/resource/${id}`}
+        >
+            <CardHeader>
+                <div>
+                    <Image
+                        src={image}
+                        className="h-full rounded-md bg-cover"
+                        alt={title}
+                        width={284}
+                        height={440}
+                    />
+                </div>
+                <CardTitle>Card Title</CardTitle>
+                <CardDescription>Card Description</CardDescription>
+            </CardHeader>
+        </Link>
+            <CardContent>
+                <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+                <p>Card Footer</p>
+            </CardFooter>
+    </Card>
+
   )
 }
 
