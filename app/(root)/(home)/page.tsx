4459ccc81a41887d18ddf9888fde267053af6703
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-29 21:58:18 +0300

    show article playlist resources

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -85,6 +85,36 @@ export default async function Home({ searchParams } : Props) {
             </section>
           )
       }
+
+      {
+        resourcesPlaylist.map((item : any) => (
+          <section
+            key={item._id}
+            className="flex-center  mt-6 w-full flex-col sm:mt-20"
+          >
+            <h1
+              className="heading3 self-start text-white-800"
+            >
+              {item.title}
+            </h1>
+            <div
+              className="mt-12 flex flex-wrap w-full justify-center gap-16 sm:justify-start"
+            >
+              {
+                item.resources.map((resource : any) => (
+                  <ResourceCard
+                    key={resource._id}
+                    title={resource.title}
+                    id={resource._id}
+                    image={resource.image}
+                    downloadNumber={resource.views}
+                  />
+                ))
+              }
+            </div>
+          </section>
+        ))
+      }
     </main>
   )
 }
