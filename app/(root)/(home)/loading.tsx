04ed163085fed833fe101617ed9b2f10b20e7477
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-30 22:20:14 +0300

    add skeleton

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -1,10 +1,32 @@
+import { Skeleton } from "@/components/ui/skeleton"
+
 import React from 'react'
 
 type Props = {}
 
 function loading({}: Props) {
   return (
-    <div>loading</div>
+    <main
+      className="flex-center paddings mx-auto w-auto max-w-screen-2xl"
+    >
+      <section
+        className="nav-padding w-full"
+      >
+        <Skeleton className="w-full h-[274px] rounded-lg" />
+      </section>
+      <section
+        className="mt-6 flex w-full flex-col sm:mt-20"
+      >
+        <Skeleton className="h-10 w-56" />
+        <div
+          className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start"
+        >
+          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+        </div>
+      </section>
+    </main>
   )
 }
 
