58fb8efd70091aa7a8a2f70ceb766acfd30c5e34
Fitiavana Sambatra <Ghostrex2@gmail.com>
2023-09-30 22:58:18 +0300

    fix skeleton color and add metadata

--- app/(root)/(home)/loading.tsx
+++ app/(root)/(home)/loading.tsx
@@ -12,18 +12,18 @@ function loading({}: Props) {
       <section
         className="nav-padding w-full"
       >
-        <Skeleton className="w-full h-[274px] rounded-lg" />
+        <Skeleton className="w-full h-[274px] rounded-lg bg-black-200/40" />
       </section>
       <section
-        className="mt-6 flex w-full flex-col sm:mt-20"
+        className="mt-6 flex w-full flex-col sm:mt-20 bg-black-200/40"
       >
         <Skeleton className="h-10 w-56" />
         <div
           className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start"
         >
-          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
-          <Skeleton className="h-[440px] w-full sm:w-[356px]" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
+          <Skeleton className="h-[440px] w-full sm:w-[356px] bg-black-200/40" />
         </div>
       </section>
     </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,11 @@ import type { Metadata } from 'next'
 export const metadata: Metadata = {
   title: 'JS Mastery ',
   description: 'JS Mastery Ressources',
+  other : {
+    "theme-color" : "#0d1117",
+    "color-scheme" : "dark-only",
+    "twitter:image" : "https://i.ibb.co/d6TXxB2/homepage-thumbnail.jpg"
+  }
 }
 
 export default function RootLayout({
